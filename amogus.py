f723007f9353c849afc41ad538f14608f4e3b3b9
Kevin <kevin.j.zhu@student.unsw.edu.au>
2022-07-09 14:50:42 +1000

    added basic stdev algo to trade. Makes profit if no brokerage, is improvement by 0.05%

--- amogus.py
+++ amogus.py
@@ -1,13 +1,18 @@
 import numpy as np
+import pandas as pd
+from scipy import stats as st
 
 nInst=100
 currentPos = np.zeros(nInst)
 
 def getMyPosition (prcSoFar):
     global currentPos
-
-    # Build your function body here
-
+    for x in range(100):
+        if prcSoFar[0].size < 30:
+            return currentPos
+        currentPos[x] = st.zscore(prcSoFar[x])[-1]
+        if np.isnan(currentPos[x] or currentPos[x] < 3):
+            currentPos[x] = 0
+        else:
+            currentPos[x] = round(currentPos[x]*100)
     return currentPos
-
-    
